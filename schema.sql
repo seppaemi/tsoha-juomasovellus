d302c57fb31c95e4b503586ada3fb32a571668bf
seppaemi <emilia.seppala@helsinki.fi>
2022-08-17 21:48:07 +0300

    fix schema

--- schema.sql
+++ schema.sql
@@ -1,55 +1,58 @@
-
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    username TEXT NOT NULL UNIQUE,
-    admin BOOLEAN DEFAULT FALSE
+    username TEXT UNIQUE,
+    password TEXT,
+    role INTEGER
 );
 
 CREATE TABLE alcohols (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    tagid INTEGER REFERENCES tags,
-    description TEXT,
-    persentage TEXT NOT NULL,
-    usage TEXT NOT NULL,
     creator_id INTEGER REFERENCES users,
-    created_at TIMESTAMP DEFAULT NOW(),
-    like_count INTEGER DEFAULT 0,
-    comment_count INTEGER DEFAULT 0,
-    visible INTEGER NOT NULL
+    created_at TIMESTAMPTZ,
+    viewed INTEGER,
+    title TEXT,
+    description TEXT,
+    notes TEXT,
+    visible INTEGER
 );
 
-CREATE TABLE likes (
+CREATE TABLE notes (
     id SERIAL PRIMARY KEY,
-    liker_id INTEGER REFERENCES users,
-    recipe_id INTEGER REFERENCES alcohols
-);
-
-CREATE TABLE comments (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    comment TEXT NOT NULL,
-    author_id INTEGER REFERENCES users,
     alcohol_id INTEGER REFERENCES alcohols,
-    created_at TIMESTAMP DEFAULT NOW(),
-    visible INTEGER NOT NULL
+    notes TEXT,
+    visible INTEGER
 );
 
 CREATE TABLE tags (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+    tag TEXT
 );
 
-
-CREATE TABLE photos (
-    id SERIAL PRIMARY KEY,
-    name TEXT, 
-    data BYTEA,
-    size int,
-    creator_id INTEGER REFERENCES users,
+CREATE TABLE alcoholtags (
     alcohol_id INTEGER REFERENCES alcohols,
-    created_at TIMESTAMP DEFAULT NOW(),
-    visible INTEGER NOT NULL
+    tag_id INTEGER REFERENCES tags,
+    visible INTEGER
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    alcohol_id INTEGER REFERENCES alcohols,
+    sender_id INTEGER REFERENCES users,
+    comment TEXT,
+    sent_at TIMESTAMPTZ,
+    visible INTEGER
+);
+
+CREATE TABLE grades (
+    id SERIAL PRIMARY KEY,
+    alcohol_id INTEGER REFERENCES alcohols,
+    grade INTEGER,
+    visible INTEGER
+);
+
+CREATE TABLE favourites (
+    user_id INTEGER REFERENCES users,
+    alcohol_id INTEGER REFERENCES alcohols,
+    added TIMESTAMPTZ,
+    visible INTEGER
 );
