fdb7e0483c498e6b07d611798607087e64d68c68
seppaemi <emilia.seppala@helsinki.fi>
2022-09-01 22:49:49 +0300

    schema fix

---
+++ schema.sql
@@ -0,0 +1,58 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    role INTEGER
+);
+
+CREATE TABLE alcohols (
+    id SERIAL PRIMARY KEY,
+    creator_id INTEGER REFERENCES users,
+    created_at TIMESTAMPTZ,
+    viewed INTEGER,
+    title TEXT,
+    description TEXT,
+    instruction TEXT,
+    visible INTEGER
+);
+
+CREATE TABLE ingredients (
+    id SERIAL PRIMARY KEY,
+    alcohol_id INTEGER REFERENCES alcohols,
+    ingredient TEXT,
+    visible INTEGER
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    tag TEXT
+);
+
+CREATE TABLE alcoholtags (
+    alcohol_id INTEGER REFERENCES alcohols,
+    tag_id INTEGER REFERENCES tags,
+    visible INTEGER
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    alcohol_id INTEGER REFERENCES alcohols,
+    sender_id INTEGER REFERENCES users,
+    comment TEXT,
+    sent_at TIMESTAMPTZ,
+    visible INTEGER
+);
+
+CREATE TABLE grades (
+    id SERIAL PRIMARY KEY,
+    alcohol_id INTEGER REFERENCES alcohols,
+    grade INTEGER,
+    visible INTEGER
+);
+
+CREATE TABLE favourites (
+    user_id INTEGER REFERENCES users,
+    alcohol_id INTEGER REFERENCES alcohols,
+    added TIMESTAMPTZ,
+    visible INTEGER
+);
